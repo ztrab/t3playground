d01098e960dbef876a39dac7015b030ce7a43b06
Bill Artz <billartzjr@gmail.com>
2023-06-12 11:49:19 -0700

    router

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,8 +7,9 @@ import Link from "next/link";
 import { api } from "~/utils/api";
 
 const Home: NextPage = () => {
-  const hello = api.example.hello.useQuery({ text: "from tRPC" });
   const user = useUser();
+
+  const { data } = api.posts.getAll.useQuery();
 
   return (
     <>
@@ -20,6 +21,11 @@ const Home: NextPage = () => {
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
         <div>{!user && <SignInButton />}</div>
         <div>{user.isSignedIn && <SignOutButton />}</div>
+        <div>
+          {data?.map((post) => (
+            <div key={post.id}>{post.content}</div>
+          ))}
+        </div>
       </main>
     </>
   );
